85f8f7b17372b5e4502e564dec212908fb5861c9
xclala <66871338+xclala@users.noreply.github.com>
2021-03-20 16:17:27 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 _ = input("上传/下载（上传 1，下载 2）")
+from os import system
 if _ == '1':
     from getpass import getpass
+    system("title 上传文件")
     port = input("端口：（默认80端口）")
     h = input("需要上传文件的主机的ip地址：（0.0.0.0表示所有主机）（默认0.0.0.0）")
     pw = getpass("密码：")
     if port == '':
         print("在浏览器中输入您的ip地址即可上传。（80端口不需要输入冒号和端口）")
     else:
-        print("在浏览器中输入您的ip地址:"+p+"即可上传。（80端口不需要输入冒号和端口）")
+        print("在浏览器中输入您的ip地址:"+port+"即可上传。（80端口不需要输入冒号和端口）")
     from flask import Flask, render_template, request
     from werkzeug.utils import secure_filename
 
@@ -35,11 +37,12 @@ if _ == '1':
             app.run(debug=False, port=80, host=h)
     else:
         if h == '':
-            app.run(debug=False, port=p, host='0.0.0.0')
+            app.run(debug=False, port=port, host='0.0.0.0')
         else:
-            app.run(debug=False, port=p, host=h)
+            app.run(debug=False, port=port, host=h)
 elif _ == '2':
     from os import system
+    system("title 下载文件")
     pp = input("端口：（默认80端口）")
     hh = input("需要下载文件的主机的ip地址：（0.0.0.0表示所有主机）（默认0.0.0.0）")
     dd = input("目录：（默认本目录）")

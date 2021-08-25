7a8bbf7d4cd4c08248276b60f6e28d8f7e653e19
xclala <xingdongxd2021@163.com>
2021-08-25 16:53:45 +0800

    优化下载文件

--- main.py
+++ main.py
@@ -1,105 +1,99 @@
-from os import system, path, urandom
-_ = input("上传/下载（上传 1，下载 2）")
+#无法上传多个文件
+from os import abort, system, path, urandom, getcwd, mkdir, listdir, walk
+from flask import Flask, render_template, request, session, send_file, abort
+from flask_wtf.csrf import CSRFProtect
+from werkzeug.utils import secure_filename
+from uuid import uuid4
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = urandom(666)
+CSRFProtect(app)
+port = input("端口：（默认80端口）")
+h = input("需要上传文件的主机的ip地址：（0.0.0.0表示所有主机）（默认0.0.0.0）")
+_ = input("让别人 上传/下载（上传输入 1，下载输入 2）")
 if _ == '1':
     from getpass import getpass
-    system("title 上传文件")
-    port = input("端口：（默认80端口）")
-    h = input("需要上传文件的主机的ip地址：（0.0.0.0表示所有主机）（默认0.0.0.0）")
     pw = getpass("密码：")
+    system("title 上传文件")
     if port == '':
         print("在浏览器中输入您的ip地址即可上传。（80端口不需要输入冒号和端口）")
     else:
-        print("在浏览器中输入您的ip地址:"+port+"即可上传。（80端口不需要输入冒号和端口）")
-    from flask import Flask, render_template, request, session
-    from flask_wtf.csrf import CSRFProtect
-    from werkzeug.utils import secure_filename
-    from uuid import uuid4
+        print("在浏览器中输入您的ip地址:" + port + "即可上传。（80端口不需要输入冒号和端口）")
+    if not path.exists(path.join(getcwd(), "别人上传的文件")):
+        mkdir("别人上传的文件")
 
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = urandom(666)
-    CSRFProtect(app)
-
-    @app.route('/')
+    @app.route('/', methods=['GET', 'POST'])
     def index():
-        return render_template('index.html')
-
-    @app.route('/', methods=['POST'])
-    def success():
         if request.method == 'POST':
-            if request.form.getlist('remember_password')==['on']:
-                if session.get("password") is None:
-                    p = request.form['passwd']
-                    if p == pw:
-                        for f in request.files.getlist('file'):
-                            session['password'] = pw
-                            if path.exists("上传的文件/"+secure_filename(f.filename)):
-                                f.save("上传的文件/"+uuid4().hex +
-                                    path.splitext(f.filename)[1])
-                            else:
-                                f.save("上传的文件/"+secure_filename(f.filename))
-                            return render_template('upload.html', alert_message="文件成功上传！")
-                    else:
-                        return render_template('index.html', alert_message="密码错误！！！")
-                else:
-                    for f in request.files.getlist('file'):
-                        if path.exists("上传的文件/"+secure_filename(f.filename)):
-                            f.save("上传的文件/"+uuid4().hex +
-                                path.splitext(f.filename)[1])
-                        else:
-                            f.save("上传的文件/"+secure_filename(f.filename))
-                        return render_template('upload.html', alert_message="文件成功上传！")
-            else:
+            if session.get("password") is None:
                 p = request.form['passwd']
                 if p == pw:
                     for f in request.files.getlist('file'):
-                        if path.exists("上传的文件/"+secure_filename(f.filename)):
-                            f.save("上传的文件/"+uuid4().hex +
-                                path.splitext(fs.filename)[1])
+                        session['password'] = pw
+                        if path.exists("别人上传的文件/" +
+                                       secure_filename(f.filename)):
+                            f.save("别人上传的文件/" + uuid4().hex +
+                                   path.splitext(f.filename)[1])
                         else:
-                            f.save("上传的文件/"+secure_filename(f.filename))
-                        return render_template('index.html', alert_message="文件成功上传！")
+                            f.save("别人上传的文件/" + secure_filename(f.filename))
+                    return render_template('upload.html',
+                                           alert_message="文件成功上传！")
                 else:
-                    return render_template('index.html', alert_message="密码错误！！！")
+                    return render_template('index.html',
+                                           alert_message="密码错误！！！")
+            else:
+                for f in request.files.getlist('file'):
+                    if path.exists("别人上传的文件/" + secure_filename(f.filename)):
+                        f.save("别人上传的文件/" + uuid4().hex +
+                               path.splitext(f.filename)[1])
+                    else:
+                        f.save("别人上传的文件/" + secure_filename(f.filename))
+                    return render_template('upload.html',
+                                           alert_message="文件成功上传！")
+        else:
+            return render_template('index.html')
 
-    if port == '':
-        if h == '':
-            app.run(debug=False, port=80, host='0.0.0.0')
-        else:
-            app.run(debug=False, port=80, host=h)
-    else:
-        if h == '':
-            app.run(debug=False, port=port, host='0.0.0.0')
-        else:
-            app.run(debug=False, port=port, host=h)
+    @app.route('/del_session', methods=['GET'])
+    def delete_session():
+        if request.method == 'GET':
+            session.clear()
+            session.pop("password", None)
+            return render_template("index.html", alert_message="成功退出登录！")
 elif _ == '2':
-    from os import system
-    system("title 下载文件")
-    pp = input("端口：（默认80端口）")
-    hh = input("需要下载文件的主机的ip地址：（0.0.0.0表示所有主机）（默认0.0.0.0）")
-    dd = input("目录：（默认本目录）")
-    if pp == '':
-        print("在浏览器中输入您的ip地址即可下载。（80端口不需要输入冒号和端口）")
+
+    @app.route('/', methods=['GET'])
+    def file_list():
+        if request.method == 'GET':
+            for ___, _________, fl in walk("让别人下载的文件"):
+                if fl != []:
+                    return render_template("filelist.html", filelist=fl)
+                else:
+                    return render_template("filelist.html", filelist="")
+
+    @app.route('/<filename>', methods=['GET'])
+    def download_file(filename):
+        if request.method == 'GET':
+            from io import BytesIO
+            from mimetypes import guess_type
+            filepath = path.join("让别人下载的文件/", filename)
+            if path.exists(filepath):
+                if path.isfile(filepath):
+                    with open(filepath, 'rb') as file_object:
+                        return send_file(BytesIO(file_object.read()),
+                                         mimetype=guess_type(filepath)[0])
+                else:
+                    abort(404)
+            else:
+                abort(404)
+
+
+if port == '':
+    if h == '':
+        app.run(debug=False, port=80, host='0.0.0.0')
     else:
-        print("在浏览器中输入您的ip地址:"+pp+"即可下载。（80端口不需要输入冒号和端口）")
-    if pp == '':
-        if hh == '':
-            if dd == '':
-                system("server 80")
-            else:
-                system(f"server --directory {dd} 80")
-        else:
-            if dd == '':
-                system(f"server --bind {hh} 80")
-            else:
-                system(f"server --bind {hh} --directory {dd} 80")
+        app.run(debug=False, port=80, host=h)
+else:
+    if h == '':
+        app.run(debug=False, port=port, host='0.0.0.0')
     else:
-        if hh == '':
-            if dd == '':
-                system(f"server {pp}")
-            else:
-                system(f"server --directory {dd} {pp}")
-        else:
-            if dd == '':
-                system(f"server --bind {hh} {pp}")
-            else:
-                system(f"server --bind {hh} --directory {dd} {pp}")
+        app.run(debug=False, port=port, host=h)

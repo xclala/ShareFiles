e8c62cec89735d8026dab2e1c600231944d4e17b
xclala <xingdongxd2021@163.com>
2021-08-25 18:39:07 +0800

    bug fix

--- main.py
+++ main.py
@@ -30,26 +30,37 @@ if _ == '1':
                 if p == pw:
                     for f in request.files.getlist('file'):
                         session['password'] = pw
+                        if f.filename != "":
+                            if path.exists("别人上传的文件/" +
+                                           secure_filename(f.filename)):
+                                f.save("别人上传的文件/" + uuid4().hex +
+                                       path.splitext(f.filename)[1])
+                            else:
+                                f.save("别人上传的文件/" +
+                                       secure_filename(f.filename))
+                        else:
+                            return render_template("index.html",
+                                                   alert_message="请先选择文件！")
+                    return render_template('upload.html',
+                                           alert_message="文件成功上传！")
+                    #这个return与for循环缩进相同，这样才能保存完多个文件后再return，进而实现多文件上传
+                else:
+                    return render_template('index.html',
+                                           alert_message="密码错误！！！")
+            else:
+                for f in request.files.getlist('file'):
+                    if f.filename != "":
                         if path.exists("别人上传的文件/" +
                                        secure_filename(f.filename)):
                             f.save("别人上传的文件/" + uuid4().hex +
                                    path.splitext(f.filename)[1])
                         else:
                             f.save("别人上传的文件/" + secure_filename(f.filename))
-                    return render_template('upload.html',
-                                           alert_message="文件成功上传！")
-                else:
-                    return render_template('index.html',
-                                           alert_message="密码错误！！！")
-            else:
-                for f in request.files.getlist('file'):
-                    if path.exists("别人上传的文件/" + secure_filename(f.filename)):
-                        f.save("别人上传的文件/" + uuid4().hex +
-                               path.splitext(f.filename)[1])
                     else:
-                        f.save("别人上传的文件/" + secure_filename(f.filename))
-                    return render_template('upload.html',
-                                           alert_message="文件成功上传！")
+                        return render_template("index.html",
+                                               alert_message="请先选择文件！")
+                return render_template('upload.html', alert_message="文件成功上传！")
+                #这个return与for循环缩进相同，这样才能保存完多个文件后再return，进而实现多文件上传
         else:
             return render_template('index.html')
 

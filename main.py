cb5ade26394086bff6de9ddaa438e60250c8c152
xclala <xingdongxd2021@163.com>
2021-10-24 13:29:00 +0800

    支持选择线程数

--- main.py
+++ main.py
@@ -8,14 +8,17 @@ app.config['SECRET_KEY'] = urandom(666)
 CSRFProtect(app)
 _ = input("让别人上传输入 1，让别人下载输入 2，既上传又下载输入 0：")  #一定要在导入upload和download之前
 port = input("端口：（默认80端口）")  #一定要在导入upload和download之前
+threads = input("线程数：（默认4个）")  #一定要在导入upload和download之前
 if _ == '1':
     if not path.exists(path.join(getcwd(), "别人上传的文件")):
         mkdir("别人上传的文件")
     from upload import app as upload
     system("title 上传文件")
+
     @app.errorhandler(404)
     def notfound(_):
         return redirect("/")
+
     app.register_blueprint(upload)
     if port == '':
         print("在浏览器中输入您的ip地址即可上传。")
@@ -45,8 +48,14 @@ elif _ == '0':
         print("在浏览器中输入您的ip地址即可上传和下载。")
     else:
         print("在浏览器中输入您的ip地址:" + port + "即可下载。")
-if port == '':
-    serve(app, host='0.0.0.0', port=80)
-
-else:
-    serve(app, host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    if port == '':
+        if threads == '':
+            serve(app, host='0.0.0.0', port=80, threads=threads)
+        else:
+            serve(app, host='0.0.0.0', port=80)
+    else:
+        if threads == '':
+            serve(app, host='0.0.0.0', port=port)
+        else:
+            serve(app, host='0.0.0.0', port=port, threads=threads)

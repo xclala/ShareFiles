6794583ba86a64be3ae25c17c5b254ffe60ed9c4
xclala <xingdongxd2021@163.com>
2023-02-05 21:23:11 +0800

    2023/02/05

--- views.py
+++ views.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session, abort, send_file, flash, redirect
+from flask import Flask, render_template, request, session, abort, send_file, flash, url_for
 from flask_wtf.csrf import CSRFProtect
 from waitress import serve
 from os import path, urandom, scandir
@@ -65,78 +65,79 @@ def secure_filename(filename):
 
     return filename
 
-
-def upload_view():
+def login():
     if request.method == 'POST':
         password = app.config['password']
-        if session.get("password") is None:
-            p = request.form['passwd']
-            if p != password:
-                flash("密码错误！！！")
-                return render_template('index.html')
-            session['password'] = password
-        if session.get("password") == password:
+        if session.get("password") == password or request.form['passwd'] == password:
+            if request.form['passwd'] == password:
+                session['password'] = request.form['passwd']
+            if app.config['mode'] == 'upload':
+                return url_for('upload_view')
+            elif app.config['mode'] == 'download':
+                return url_for('filelist')
+            else:
+                ...
+                #之后继续编既上传又下载的情况
+        elif session.get(
+                "password") != password or request.form['passwd'] != password:
+            flash("密码错误！")
+    return render_template('login.html')
+
+def upload_view():
+    if session.get("password") == app.config['password']:
+        if request.method == 'POST':
             for f in request.files.getlist('file'):
                 if secure_filename(f.filename) == "":
                     flash("请先选择文件！")
-                    return render_template("index.html")
+                    return render_template("upload.html")
                 if path.exists("共享的文件/" + secure_filename(f.filename)):
                     f.save("共享的文件/" + uuid4().hex +
-                           path.splitext(f.filename)[1])
+                            path.splitext(f.filename)[1])
                 else:
                     f.save("共享的文件/" + secure_filename(f.filename))
             flash("文件成功上传！")
-            return render_template('upload.html')
-        flash("密码错误！！！")
-    return render_template('index.html')
+        return render_template('upload.html')
+    return url_for('login')
 
 
 def delete_session():
     session.clear()
     session.pop("password", None)
     flash("成功退出登录！")
-    return redirect("/")
+    return url_for('login')
 
 
 def filelist():
-    if request.method == 'POST':
-        password = app.config['password']
-        if session.get(
-                "password") == password or request.form['passwd'] == password:
-            if request.form['passwd'] == password:
-                session['password'] = request.form['passwd']
-            filelist = []
-            for fl in scandir("共享的文件"):
-                if fl.is_file():
-                    filelist.append(fl.name)
-            return render_template("download.html", filelist=filelist)
-        elif session.get(
-                "password") != password or request.form['passwd'] != password:
-            flash("密码错误！")
-    return render_template("filelist.html", filelist='')
+    if session.get("password") == app.config['password']:
+        filelist = []
+        for fl in scandir("共享的文件"):
+            if fl.is_file():
+                filelist.append(fl.name)
+        return render_template("download.html", filelist=filelist)
+    return url_for('login')
 
 
 def download_file(filename):
-    password = app.config['password']
-    if password == session.get('password'):
+    if app.config['password'] == session.get('password'):
         filepath = path.join("共享的文件/", secure_filename(filename))
         if path.exists(filepath) and path.isfile(filepath):
             return send_file(filepath)
         abort(404)
-    flash("密码错误！")
-    return render_template("filelist.html", filelist='')
+    return url_for('login')
 
 
 def upload(port, thread, pw):
     app.config['password'] = pw
-    app.add_url_rule('/', view_func=upload_view, methods=['GET', 'POST'])
+    app.config['mode'] = 'upload'
+    app.add_url_rule('/', view_func=login, methods=['GET', 'POST'])
     app.add_url_rule('/del_session', view_func=delete_session, methods=['GET'])
     serve(app, port=port, threads=thread)
 
 
 def download(port, thread, pw):
     app.config['password'] = pw
-    app.add_url_rule("/", view_func=filelist, methods=['GET', 'POST'])
+    app.config['mode'] = 'download'
+    app.add_url_rule("/", view_func=login, methods=['GET', 'POST'])
     app.add_url_rule("/filelist/<filename>",
                      view_func=download_file,
                      methods=['GET'])
@@ -145,8 +146,12 @@ def download(port, thread, pw):
 
 
 def upload_download(port, thread, pw):
-    app.add_url_rule("/filelist", view_func=filelist, methods=['GET', 'POST'])
+    app.config['password'] = pw
+    app.config['mode'] = 'upload_download'
+    app.add_url_rule('/', view_func=login, methods=['GET', 'POST'])
     app.add_url_rule("/filelist/<filename>",
-                     view_func=download_file,
-                     methods=['GET'])
-    upload(port, thread, pw)
+                    view_func=download_file,
+                    methods=['GET'])
+    app.add_url_rule('/del_session', view_func=delete_session, methods=['GET'])
+    serve(app, port=port, threads=thread)
+#upload() download() upload_download() 这3个函数之后用类重构

28b20e6e9f388f00b6e4d5716f60cbf073b99630
xclala <xingdongxd2021@163.com>
2023-02-06 12:24:18 +0800

    2023/02/06

--- views.py
+++ views.py
@@ -71,23 +71,21 @@ def login():
         if session.get("password") == password or request.form['passwd'] == password:
             if request.form['passwd'] == password:
                 session['password'] = request.form['passwd']
-            if app.config['mode'] == 'upload':
-                return redirect(url_for('upload_view'))
-            elif app.config['mode'] == 'download':
-                return redirect(url_for('filelist'))
+            if app.config['mode'] == 'download':
+                return redirect(url_for('filelist_view'))
             else:
-                ...
-                #之后继续编既上传又下载的情况
+                return redirect(url_for('upload_view'))
         elif session.get(
                 "password") != password or request.form['passwd'] != password:
             flash("密码错误！")
     return render_template('login.html')
 
 def upload_view():
+    #出现400 Bad Request
     if session.get("password") == app.config['password']:
         if request.method == 'POST':
             for f in request.files.getlist('file'):
-                if secure_filename(f.filename) == "":
+                if not secure_filename(f.filename):
                     flash("请先选择文件！")
                     return render_template("upload.html")
                 if path.exists("共享的文件/" + secure_filename(f.filename)):
@@ -96,7 +94,9 @@ def upload_view():
                 else:
                     f.save("共享的文件/" + secure_filename(f.filename))
             flash("文件成功上传！")
-        return render_template('upload.html')
+        if app.config['mode'] == 'upload':
+            return render_template('upload.html')
+        return render_template('upload.html', filelist=filelist(), title="共享文件")
     return redirect(url_for('login'))
 
 
@@ -106,14 +106,16 @@ def delete_session():
     flash("成功退出登录！")
     return redirect(url_for('login'))
 
-
 def filelist():
+    filelist = []
+    for fl in scandir("共享的文件"):
+        if fl.is_file():
+            filelist.append(fl.name)
+    return filelist
+
+def filelist_view():
     if session.get("password") == app.config['password']:
-        filelist = []
-        for fl in scandir("共享的文件"):
-            if fl.is_file():
-                filelist.append(fl.name)
-        return render_template("download.html", filelist=filelist)
+        return render_template("download.html", filelist=filelist())
     return redirect(url_for('login'))
 
 
@@ -139,7 +141,7 @@ def download(port, thread, pw):
     app.config['password'] = pw
     app.config['mode'] = 'download'
     app.add_url_rule("/", view_func=login, methods=['GET', 'POST'])
-    app.add_url_rule('/filelist', view_func=filelist, methods=['GET', 'POST'])
+    app.add_url_rule('/filelist', view_func=filelist_view, methods=['GET', 'POST'])
     app.add_url_rule("/filelist/<filename>",
                      view_func=download_file,
                      methods=['GET'])

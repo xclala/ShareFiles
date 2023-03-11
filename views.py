9138458ea6537362ae24989a34e2b4ca1b633299
X.D <xingdongxd2021@163.com>
2023-03-11 15:28:12 +0800

    Update views.py

--- views.py
+++ views.py
@@ -4,7 +4,7 @@ from secrets import token_urlsafe
 from uuid import uuid4
 from datetime import timedelta
 from pathlib import Path
-from typing import Any, Optional
+from typing import Optional
 
 app: Flask = Flask(__name__)
 app.config['SECRET_KEY'] = token_urlsafe(64)
@@ -67,7 +67,7 @@ def secure_filename(filename: str | Path | None) -> str:
 
 
 @app.before_request
-def is_login() -> Any:
+def is_login():
     if request.path == url_for('login'):
         return None
     if request.path == url_for('static', filename='style.css'):
@@ -79,7 +79,7 @@ def is_login() -> Any:
     return redirect(url_for('login'))
 
 
-def login() -> Any:
+def login():
     if session.get("password") == app.config['password']:
         if app.config['mode'] == 'download':
             return redirect(url_for('filelist', filepath=''))
@@ -99,7 +99,7 @@ def login() -> Any:
     return render_template('login.html')
 
 
-def upload_view() -> Any:
+def upload_view():
     if request.method == 'POST':
         for f in request.files.getlist('file'):
             if not f.filename:
@@ -118,14 +118,14 @@ def upload_view() -> Any:
     return render_template('upload.html', filelist=app.config['dir'].iterdir(), title="共享文件")
 
 
-def delete_session() -> Any:
+def delete_session():
     session.clear()
     session.pop("password", None)
     flash("成功退出登录！")
     return redirect(url_for('login'))
 
 
-def filelist(filepath: str) -> Any:
+def filelist(filepath: str):
     path: Path = app.config['dir'] / filepath.replace("..", "")
     if path.is_file():
         return send_from_directory(app.config['dir'],
@@ -138,7 +138,7 @@ def filelist(filepath: str) -> Any:
     abort(404)
 
 
-def delete_file(filepath: str) -> Any:
+def delete_file(filepath: str):
     #之后实现回收站
     from shutil import rmtree
     if app.config['file_can_be_deleted']:
@@ -156,7 +156,7 @@ def delete_file(filepath: str) -> Any:
     return redirect("/")
 
 
-def newfile() -> Any:
+def newfile():
     #之后让它支持文件夹
     if request.method == 'POST':
         if secure_filename(request.form['filename']):
@@ -169,7 +169,7 @@ def newfile() -> Any:
     return render_template('newfile.html')
 
 
-def edit(filename: str) -> Any:
+def edit(filename: str):
     #之后让它支持文件夹
     if app.config['mode'] == 'upload_download':
         filename = secure_filename(filename)

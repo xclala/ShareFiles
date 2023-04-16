3dd1ea37b2435baae01f74abbecc3e7f10551cb0
xclala <xingdongxd2021@163.com>
2023-04-16 21:26:37 +0800

    refactor(app.py):重构询问路径

--- app.py
+++ app.py
@@ -10,38 +10,25 @@ from platform import python_version_tuple
 assert python_version_tuple()[0] == '3' and int(
     python_version_tuple()[1]) > 10, "python版本太低，无法运行"
 
-dir: Path = Path()
 parser: ArgumentParser = ArgumentParser()
-parser.add_argument('-p', '--port', type=int)
-parser.add_argument('-t', '--thread', type=int)
+parser.add_argument('--port', type=int)
+parser.add_argument('--thread', type=int)
+parser.add_argument('--path', type=str)
 parser.add_argument('--upload', action="store_true")
 parser.add_argument('--download', action="store_true")
 parser.add_argument('--debug', action="store_true")
 parser.add_argument('--file_can_be_deleted', action="store_true")
-delete_permission: bool | BooleanVar = parser.parse_args(
-).file_can_be_deleted
+delete_permission: bool | BooleanVar = parser.parse_args().file_can_be_deleted
 debug_mode: bool = parser.parse_args().debug
 port: int = parser.parse_args().port
 threads: int = parser.parse_args().thread
+dir: str = parser.parse_args().path
 upload: bool = parser.parse_args().upload
 download: bool = parser.parse_args().download
 if not parser.parse_args().upload and not parser.parse_args().download:
-    def ask_dir():
-        global dir
-        path: str = askdirectory()
-        if path:
-            dir = Path(path)
-        else:
-            try:
-                Path("共享的文件").mkdir()
-            except FileExistsError:
-                ...
-            dir = Path("共享的文件")
-        showinfo("已选择文件夹", "已选择文件夹！")
-
     root: Tk = Tk()
     root.title("请选择")
-    root.geometry("300x300")
+    root.geometry("400x400")
     root.resizable(False, False)
     if port is None:
         Label(text="端口：").pack()
@@ -55,26 +42,30 @@ if not parser.parse_args().upload and not parser.parse_args().download:
         t.pack()
         t.insert(0, "6")
         threads = int(t.get())
-    var1: BooleanVar = BooleanVar()
-    var2: BooleanVar = BooleanVar()
+    upload_temp: BooleanVar = BooleanVar()
+    download_temp: BooleanVar = BooleanVar()
     pw_temp: StringVar = StringVar()
-    var1.set(True)
-    var2.set(True)
-    Checkbutton(root, text="允许他人更改“共享的文件”文件夹", variable=var1).pack()
-    Checkbutton(root, text="允许他人访问“共享的文件”文件夹", variable=var2).pack()
+    path_temp: StringVar = StringVar()
+    upload_temp.set(True)
+    download_temp.set(True)
+    Checkbutton(text="允许他人更改“共享的文件”文件夹", variable=upload_temp).pack()
+    Checkbutton(text="允许他人访问“共享的文件”文件夹", variable=download_temp).pack()
     if not delete_permission:
         delete_permission = BooleanVar()
-        Checkbutton(root,
-                    text="允许他人删除“共享的文件”文件夹中的文件",
+        Checkbutton(text="允许他人删除“共享的文件”文件夹中的文件",
                     variable=delete_permission).pack()
     Label(text="密码：").pack()
     Entry(textvariable=pw_temp, show="*").pack()
-    Label(text="文件夹：").pack()
-    Button(root, text="选择文件夹", command=ask_dir).pack()
+    if dir is None:
+        Label(text="文件夹：").pack()
+        Entry(textvariable=path_temp).pack()
+        Button(text="选择文件夹",
+               command=lambda: path_temp.set(askdirectory())).pack()
     Button(root, text="确定", command=root.destroy).pack()
     mainloop()
-    upload: bool = var1.get()
-    download: bool = var2.get()
+    upload: bool = upload_temp.get()
+    download: bool = download_temp.get()
+    dir: str = path_temp.get()
 if port is None or port <= 0 or port >= 65535:
     port = 80
 if threads is None or threads <= 0:
@@ -93,9 +84,15 @@ if download:
         showinfo("", f"在浏览器中输入您的ip地址:{port}即可允许他人访问{dir}")
     app = register_download()
     app.config['delete_permission'] = delete_permission.get()
-if dir == Path():
-    dir = Path("共享的文件")
-app.config['dir'] = dir
+if dir:
+    directory: Path = Path(dir)
+else:
+    try:
+        Path("共享的文件").mkdir()
+    except FileExistsError:
+        ...
+    directory: Path = Path("共享的文件")
+app.config['dir'] = directory
 app.config['upload'] = upload
 app.config['download'] = download
 app.config['password'] = pw_temp.get()

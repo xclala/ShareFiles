87b63bd138c34d02438eb4186055d7c40d8cd959
xclala <xingdongxd2021@163.com>
2023-08-17 17:09:14 +0800

    bugfix

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # ShareFiles
 共享文件的源代码  
-usage: app.py [-h] [-p PORT] [-t THREAD] [--upload] [--download] [--debug][--file_can_be_deleted]
+usage: app.py [-h] [-p PORT] [-t THREAD] [--upload] [--download] [--debug][--file_can_be_deleted] [--path]
 
 options:
   -h, --help  show this help message and exit  
   -p PORT, --port PORT  
-  -t THREAD, --thread THREAD  
+  -t THREAD, --thread THREAD
+  --path PATH  
   --upload  
   --download  
   --debug  
--- app.py
+++ app.py
@@ -25,47 +25,48 @@ threads: int = parser.parse_args().thread
 dir: str = parser.parse_args().path
 upload: bool = parser.parse_args().upload
 download: bool = parser.parse_args().download
+
+root: Tk = Tk()
+root.title("请选择")
+root.geometry("400x400")
+root.resizable(False, False)
+if port is None:
+    Label(text="端口：").pack()
+    p: Entry = Entry()
+    p.pack()
+    p.insert(0, "80")
+    port = int(p.get())
+if threads is None:
+    Label(text="线程数：").pack()
+    t: Entry = Entry()
+    t.pack()
+    t.insert(0, "6")
+    threads = int(t.get())
+upload_temp: BooleanVar = BooleanVar()
+download_temp: BooleanVar = BooleanVar()
+pw_temp: StringVar = StringVar()
+path_temp: StringVar = StringVar()
+upload_temp.set(True)
+download_temp.set(True)
 if not parser.parse_args().upload and not parser.parse_args().download:
-    root: Tk = Tk()
-    root.title("请选择")
-    root.geometry("400x400")
-    root.resizable(False, False)
-    if port is None:
-        Label(text="端口：").pack()
-        p: Entry = Entry()
-        p.pack()
-        p.insert(0, "80")
-        port = int(p.get())
-    if threads is None:
-        Label(text="线程数：").pack()
-        t: Entry = Entry()
-        t.pack()
-        t.insert(0, "6")
-        threads = int(t.get())
-    upload_temp: BooleanVar = BooleanVar()
-    download_temp: BooleanVar = BooleanVar()
-    pw_temp: StringVar = StringVar()
-    path_temp: StringVar = StringVar()
-    upload_temp.set(True)
-    download_temp.set(True)
     Checkbutton(text="允许他人更改“共享的文件”文件夹", variable=upload_temp).pack()
     Checkbutton(text="允许他人访问“共享的文件”文件夹", variable=download_temp).pack()
-    if not delete_permission:
-        delete_permission = BooleanVar()
-        Checkbutton(text="允许他人删除“共享的文件”文件夹中的文件",
-                    variable=delete_permission).pack()
-    Label(text="密码：").pack()
-    Entry(textvariable=pw_temp, show="*").pack()
-    if dir is None:
-        Label(text="文件夹：").pack()
-        Entry(textvariable=path_temp).pack()
-        Button(text="选择文件夹",
-               command=lambda: path_temp.set(askdirectory())).pack()
-    Button(root, text="确定", command=root.destroy).pack()
-    mainloop()
     upload: bool = upload_temp.get()
     download: bool = download_temp.get()
+if not delete_permission:
+    delete_permission = BooleanVar()
+    Checkbutton(text="允许他人删除“共享的文件”文件夹中的文件",
+                variable=delete_permission).pack()
+Label(text="密码：").pack()
+Entry(textvariable=pw_temp, show="*").pack()
+if dir is None:
+    Label(text="文件夹：").pack()
+    Entry(textvariable=path_temp).pack()
+    Button(text="选择文件夹",
+            command=lambda: path_temp.set(askdirectory())).pack()
     dir: str = path_temp.get()
+Button(root, text="确定", command=root.destroy).pack()
+mainloop()
 if port is None or port <= 0 or port >= 65535:
     port = 80
 if threads is None or threads <= 0:
